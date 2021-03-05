f269beb68de4fee0bc55d1860fa2cabf6372ba39
s2056508 <l.chen-81@sms.ed.ac.uk>
2021-03-05 17:58:32 +0000

    sorted search

--- search.py
+++ search.py
@@ -106,10 +106,65 @@ def normal_search(input, review_dic):
     result_dic = {}
     for movie in suitable_movies:
         result_dic[movie] = review_dic[movie]
+        # print(result_dic[movie])
     start_2 = datetime.datetime.now()
     print(start_2 - start_0)
 
     return result_dic
+
+
+# List[5]; List[0] = movie name, List[1] = movie year, List[2] = average rating, List[3] = genre,
+# List[4] = the total number of reviews
+def movie_result(result_dic, type='popularity'):
+    all_movie_result = []
+    for movie, review in result_dic.items():
+        # if len(review) == 1:
+        for year, review_info in review.items():
+            single_movie_result = []
+            single_movie_result.append(movie)
+            if year == '\\N':
+                year = '0'
+            single_movie_result.append(int(year))
+            valid_rating = []
+            for single_review in review_info[2]:
+                rating = single_review['rating']
+                # print(rating)
+                if rating is not None:
+                    valid_rating.append(int(rating))
+            if len(valid_rating) == 0:
+                average_rating = 0
+            else:
+                average_rating = round(sum(valid_rating)/len(valid_rating),2)
+                # print(valid_rating)
+                # print(len(valid_rating))
+            # print(round(average_rating,2))
+            single_movie_result.append(average_rating)
+            single_movie_result.append(review_info[1])
+            single_movie_result.append(len(review_info[2]))
+
+            # print(single_movie_result)
+            all_movie_result.append(single_movie_result)
+    # print(len(all_movie_result))
+    all_movie_result = sort_result(all_movie_result, type)
+    return all_movie_result
+
+
+# 根据type进行排序
+def sort_result(result_list, type):
+    temp_dic = {}
+    for i in range(len(result_list)):
+        temp_dic[i] = result_list[i]
+    if type == "popularity":
+        temp_dic = sorted(temp_dic.items(), key = lambda d: d[1][4], reverse=True)
+    elif type == 'year':
+        temp_dic = sorted(temp_dic.items(), key = lambda d: d[1][1], reverse=True)
+    elif type == 'rating':
+        temp_dic = sorted(temp_dic.items(), key = lambda d: d[1][2], reverse=True)
+
+    sorted_list = list(dict(temp_dic).values())
+    # print(temp_dic)
+    # print(sorted_list)
+    return sorted_list
 
 
 # Edit this function to get the users' input
@@ -126,11 +181,24 @@ if __name__ == "__main__":
     with open(result_file) as f:
         review_dic = json.load(f)
 
+    # 影评分类的测试
+    get_movie = "hollywood"
+    result_dic = normal_search(get_movie, review_dic)
+
+    all_movie_result = movie_result(result_dic)
+    print(all_movie_result)
+    example = ['popularity', 'year', 'rating']
+    for a in example:
+        print(a)
+        all_movie_result = movie_result(result_dic, a)
+        print(all_movie_result)
+
     # A while loop in order to save time of reading json file
     ifSearch = True
     while ifSearch:
         # input_movie = input("Please input the movie name: ")
         input_movie = get_input_movie()
+        print(input_movie)
         if input_movie == 'q':
             ifSearch = False
         else:
@@ -138,8 +206,10 @@ if __name__ == "__main__":
             # for word in input.split():
             #     word = spell.correction(word)
             correct_input = ' '.join([spell.correction(word) for word in input_movie.split()])
+            print(correct_input)
             # print(correct_input)
             result_dic = normal_search(correct_input, review_dic)
+            all_movie_result = movie_result(result_dic)
 
 
 
