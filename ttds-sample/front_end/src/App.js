fe23eb85f30082e9da58f924cb365711a603600f
s2056508 <l.chen-81@sms.ed.ac.uk>
2021-03-30 13:33:05 +0100

    App and MyRouter.js update

--- ttds-sample/front_end/src/App.js
+++ ttds-sample/front_end/src/App.js
@@ -1,18 +1,12 @@
 
 import React, {Component} from 'react';
-import MainPage from './pages/MainPage';
-import ReviewOverall from './pages/ReviewOverall';
 import Router from "./MyRouter";
- 
- 
 import 'antd/dist/antd.css';
 import './App.css';
  
 class App extends Component{
   render() {
     return (
-      // <MainPage />
-      // <ReviewOverall />
       <Router />
     );
   }
--- ttds-sample/front_end/src/MyRouter.js
+++ ttds-sample/front_end/src/MyRouter.js
@@ -5,6 +5,7 @@ import {HashRouter, withRouter, Route, Redirect} from 'react-router-dom';
 import MainPage from "./pages/MainPage";
 import ReviewOverall from "./pages/ReviewOverall";
 import MoviePage from "./pages/MoviePage"
+import MovieReviews from "./pages/MovieReviews"
 
 export default class MyRouter extends Component {
     render(){
@@ -12,9 +13,9 @@ export default class MyRouter extends Component {
             <HashRouter>
                 <div>
                     <Route exact path='/' component={MainPage}/>
-                    {/* <Route path="/MainPage" component={MainPage}/> */}
                     <Route path="/ReviewOverall" component={ReviewOverall}/>
                     <Route path="/MoviePage" component={MoviePage}/>
+                    <Route path='/MovieReviews' component={MovieReviews}/>
                 </div>
             </HashRouter>
         )
